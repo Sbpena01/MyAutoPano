8a5ac988b212db925087b8019b023ac798939360
Scott Pena <scottpena@autoreg-6691809.dyn.wpi.edu>
2025-01-29 17:13:09 -0500

    Data Generation, must continue to verify

--- Phase2/Code/DataGeneration.py
+++ Phase2/Code/DataGeneration.py
@@ -1,6 +1,57 @@
 import argparse
+import cv2
+import os
+import numpy as np
+import random
+from Utilities import Point, Bounding_Box 
 
 DEBUG_LEVEL = 0
+PAIR_COUNT = 0
+PERTURBATION_RANGE = 10
+def load_images(im_path: str, num_images: int, flags: int = cv2.IMREAD_GRAYSCALE) -> tuple[list[cv2.Mat], list[str]]:
+    images = []
+    image_names = []
+    filenames = os.listdir(im_path)
+    filenames.sort(key=lambda x: int(x.split('.')[0]))  # Sort based on the number before the '.'
+    count = 0
+    for filename in filenames:
+        full_image_path = im_path+filename
+        image = cv2.imread(full_image_path, flags=flags)
+        image_names.append(filename)
+        images.append(image)
+        count +=1
+        if count == num_images:
+            break
+    return images, image_names
+
+def generate_patch(image: np.ndarray, patch_size = 101):
+    # We generate a random patch by randomly selecting a point to act at the top left corner of
+    # patch. We first calculate the active region based on the perturbation range and the
+    # patch size.
+    image_width = image.shape[1]
+    image_height = image.shape[0]
+    #                         Lower bound         Upper bound
+    random_x = random.randint(PERTURBATION_RANGE, image_width - (PERTURBATION_RANGE + patch_size))
+    random_y = random.randint(PERTURBATION_RANGE, image_height - (PERTURBATION_RANGE + patch_size))
+    
+    # Build the Bounding_Box object to represent the generated patch.
+    top_left = Point((random_y, random_x))
+    top_right = Point((random_y, random_x+patch_size))
+    bottom_left = Point((random_y+patch_size, random_x))
+    bottom_right = Point((random_y+patch_size, random_x+patch_size))
+    return Bounding_Box(top_left, top_right, bottom_left, bottom_right)
+
+
+def perterbate_patch(bounding_box: Bounding_Box) -> Bounding_Box:
+    new_bb_list = []
+    for point in bounding_box.get_points():
+        new_bb_list.append( Point((  (point.y) + random.randint(-PERTURBATION_RANGE, PERTURBATION_RANGE),
+                                    (point.x) + random.randint(-PERTURBATION_RANGE, PERTURBATION_RANGE))))
+    new_bb = Bounding_Box(new_bb_list[0],new_bb_list[1],new_bb_list[2],new_bb_list[3])
+
+    if new_bb == bounding_box:
+        new_bb = perterbate_patch(bounding_box)
+    return new_bb
 
 def main():
     Parser = argparse.ArgumentParser()
@@ -9,8 +60,8 @@ def main():
     """
     Parser.add_argument(
         "--ImagePath",
-        default="",  # TODO: Add default when dataset is added to repo.
-        help="Base path of images, Default: TODO",
+        default="Phase2/Data/Train/",
+        help="Base path of images, Default: Phase2/Data/Train",
     )
     Parser.add_argument(
         "--OutputPath",
@@ -24,13 +75,111 @@ def main():
         default=0,
         help="Increase debug verbosity with higher debug level"
     )
+    
+    Parser.add_argument(
+        "--PairCount",
+        type=int,
+        default=5,
+        help="Increase debug verbosity with higher debug level"
+    )
+    
+    Parser.add_argument(
+        "--NumImages",
+        type=int,
+        default=20,
+        help="Increase debug verbosity with higher debug level"
+    )
 
     Args = Parser.parse_args()
     ImagePath = Args.ImagePath
     OutputPath = Args.OutputPath
-    global DEBUG_LEVEL
+    NumImages = Args.NumImages
+    global DEBUG_LEVEL, PAIR_COUNT
     DEBUG_LEVEL = Args.DebugLevel
+    PAIR_COUNT = Args.PairCount
     
+
+    """ Read a set of images from input directory """
+    image_set, image_names = load_images(ImagePath, NumImages, cv2.IMREAD_ANYCOLOR)
+
+    """ Generate a sub patch within bounds [(x_min,y_min), (x_man, y_max)] (P_a) """
+    
+    test_image = image_set[0]
+    test_name = image_names[0]
+
+
+    unwarped_bb = generate_patch(test_image)
+    unwarped_patch = test_image[unwarped_bb.tl.y:unwarped_bb.bl.y, unwarped_bb.tl.x:unwarped_bb.br.x]
+    print(f"unwarped bb {unwarped_bb}\n")
+    # cv2.imshow('original image', test_image)
+    # cv2.imshow('unwarped_patch', unwarped_patch)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    """ Randomly translate each corner on sub patch (P_b)"""
+    warped_bb = perterbate_patch(unwarped_bb)
+    print(f"warped bb {warped_bb}")
+     
+    homography = cv2.getPerspectiveTransform(unwarped_bb.get_points_np(), warped_bb.get_points_np())
+    homography_inv = np.linalg.inv(homography)
+
+
+    print(homography_inv)
+    corners = np.array([
+                [0, 0],
+                [0, test_image.shape[0]],
+                [test_image.shape[1], 0],
+                [test_image.shape[1], test_image.shape[0]]
+            ], dtype=np.float32)
+
+    # Convert corners to homogeneous coordinates
+    corners = np.column_stack((corners, np.ones(corners.shape[0])))
+
+    # Apply the homography matrix
+    transformed_corners = np.dot(homography_inv, corners.T) 
+
+    # Normalize the points to convert back from homogeneous coordinates
+    transformed_corners /= transformed_corners[2]
+
+    # Extract x and y coordinates
+    x_coords = transformed_corners[0]
+    y_coords = transformed_corners[1]
+
+    x_min, x_max = int(np.min(x_coords)), int(np.max(x_coords))
+    y_min, y_max = int(np.min(y_coords)), int(np.max(y_coords))
+
+    width = max(x_max, test_image.shape[1]) - min(0, x_min)
+    height = max(y_max,test_image.shape[0]) - min(0, y_min)
+    dsize = (width, height)
+
+    offset_x = -x_min if x_min < 0 else 0
+    offset_y = -y_min if y_min < 0 else 0
+
+    offset_matrix = np.array([
+        [1, 0, offset_x],
+        [0, 1, offset_y],
+        [0, 0, 1]
+    ], dtype=np.float64)
+
+    # Update the homography
+    H_offset = np.dot(offset_matrix, homography_inv)
+
+    warped_image = cv2.warpPerspective(test_image, M=H_offset, dsize=dsize)
+    warped_patch = warped_image[(unwarped_bb.tl.y+offset_y):(unwarped_bb.bl.y+offset_y),
+                                 (unwarped_bb.tl.x+offset_x):(unwarped_bb.br.x+offset_x)]
+
+    cv2.imshow('warped image', warped_image)
+    cv2.imshow('warped patch', warped_patch)
+    cv2.imshow('unwarped patch', unwarped_patch)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+    """ Use Inverse of H (H^b_a) to transform image and generate warped Subpatch """
+
+    """" Stack image frames (data_out) to a file, and generate corresponding label H_4pt """
+
     return
 
 
---
+++ Phase2/Code/Utilities.py
@@ -0,0 +1,39 @@
+import numpy as np
+class Point:
+    def __init__(self, point: tuple):
+        self.x = int(point[1])
+        self.y = int(point[0])
+
+    # WITH format (X,Y) NOT (Y,X)
+    def to_numpy(self):
+        return np.array([self.x, self.y])
+
+    def __eq__(self, other: "Point"):
+        return (self.x == other.x) and (self.y == other.y)
+
+class Bounding_Box:
+    def __init__(self, tl: Point, tr:Point, bl:Point, br:Point):
+        self.tl = tl
+        self.tr = tr
+        self.bl = bl
+        self.br = br
+
+    def get_points(self) -> list[Point]:
+        return [self.tl, self.tr, self.bl, self.br]
+    
+    def get_points_np(self) -> np.ndarray:
+        return np.array([   [self.tl.x, self.tl.y], 
+                            [self.tr.x, self.tr.y],
+                            [self.bl.x, self.bl.y],
+                            [self.br.x, self.br.y]], dtype='float32')
+
+    def __str__(self):
+        return f"tl: ({self.tl.x}, {self.tl.y}), tr: ({self.tr.x}, {self.tr.y}),\n\t bl: ({self.bl.x}, {self.bl.y}), br: ({self.br.x}, {self.br.y})   "
+    
+    def __eq__(self, other: "Bounding_Box"):
+        for point_1, point_2 in zip(self.get_points(), other.get_points()):
+            if not point_1 == point_2:
+                return False
+        return True 
+
+    
