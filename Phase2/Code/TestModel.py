dd5ee298b5303e77768aa63d302c8d3016fb3321
riblair <rileypatblair@gmail.com>
2025-02-09 09:58:35 -0500

    model output looking good:

--- Phase2/Code/TestModel.py
+++ Phase2/Code/TestModel.py
@@ -11,13 +11,14 @@ import numpy as np
 def main():
     # load model 
     device = torch.device('cuda')
+    device_cpu = torch.device('cpu')
     model = HomographyModel('Sup')
-    model.to(device)
+    # model.to(device)
     model.eval()
 
-    Checkpoint = torch.load("Phase2/Checkpoints/14model.ckpt", map_location=device)
-
-    model.load_state_dict(Checkpoint["model_state_dict"])
+    # Checkpoint = torch.load("Phase2/Checkpoints/49modelcpu.ckpt", map_location=device_cpu)
+    # model.load_state_dict(Checkpoint["model_state_dict"])
+    model.load_state_dict(torch.load("Phase2/Checkpoints/49modelcpu.ckpt", weights_only=True), strict=False)
     
     idx = 1
     image = cv2.imread(f"Phase2/Data/Val/{idx}.jpg", cv2.IMREAD_GRAYSCALE)
@@ -32,7 +33,7 @@ def main():
     warped_patch = warped_image[int(original_patch.tl.y+offset_matrix[1,2]):int(original_patch.bl.y+offset_matrix[1,2]),
                                         int(original_patch.tl.x+offset_matrix[0, 2]):int(original_patch.br.x+offset_matrix[0,2])]
 
-    # DataGeneration.display_bounding_boxes('unwarped image w/ ground truth', image, (original_patch, perturbed_patch))
+    DataGeneration.display_bounding_boxes('unwarped image w/ Origin and GT', image, (original_patch, perturbed_patch))
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
@@ -40,7 +41,7 @@ def main():
 
     patches_model = torch.from_numpy(np.array([[unwarped_patch, warped_patch]], dtype=np.float32))
     print(patches_model.shape)
-    patches_model = patches_model.to(device)
+    # patches_model = patches_model.to(device)
     with torch.no_grad():
         output = model(patches_model)
     
@@ -50,36 +51,29 @@ def main():
     print(H_4pt)
     print(output)
 
-    output = output.numpy()
+    output = output.numpy().reshape((4,2))
 
-    src = np.float32([[0, 0], [0, 128], [128, 0], [128, 128]])
-
-    # Destination points
-    dst = output.reshape((4,2))
-    dst = src + dst
-
-    # print(dst)
-
-    # H_output = cv2.getPerspectiveTransform(src, dst)
-    H_output = Utilities.find_homography(src, dst)
-
+    
+    homography_estim = Utilities.find_homography(original_patch.get_points_np(), original_patch.get_points_np()+output)
+    homography_estim_inv = np.linalg.inv(homography_estim)
+    dsize_estim, offset_matrix_estim = Utilities.calculate_dsize(image, homography_estim_inv)
+    homography_estim_offset = np.dot(offset_matrix_estim, homography_estim_inv)
+    warped_image = cv2.warpPerspective(image, homography_estim_offset, dsize=dsize_estim)
     print(homography)
-    print(H_output)
-    exit(1)
+    print("\n")
+    print(homography_estim)
+ 
+    warped_bb_estim = DataGeneration.warp_bb(original_patch, homography_estim)
 
-
-    warped_output_bb = DataGeneration.warp_bb(original_patch, H_output)
-
-    # print(warped_output_bb)
-    dsize_output, offset_matrix_output = Utilities.calculate_dsize(image, H_output)
-
-    H_output_2 = np.dot(offset_matrix_output, H_output)
-    # DataGeneration.display_bounding_boxes('unwarped image w/ ground truth', image, (perturbed_patch, warped_output_bb))
-    warped_image = cv2.warpPerspective(image, H_output_2, dsize=dsize_output)
-    DataGeneration.display_bounding_boxes(' Warped Image w/ ground truth', image, 
+    # H_output_inv = np.linalg.inv(H_output)
+    # # H_output_forward = np.dot(offset_matrix_output, H_output)
+    # H_output_inv = np.dot(offset_matrix_output, H_output_inv)
+    DataGeneration.display_bounding_boxes('unwarped image w/ GT and Estim', image, (perturbed_patch, warped_bb_estim))
+    
+    DataGeneration.display_bounding_boxes(' Warped Image w/ GT and Estim', warped_image, 
                                           (DataGeneration.warp_bb(perturbed_patch, H_offset), 
-                                           DataGeneration.warp_bb(warped_output_bb, H_output_2)))
-    cv2.imshow('after homography net', warped_image)
+                                           DataGeneration.warp_bb(warped_bb_estim, homography_estim_offset)))
+    # cv2.imshow('after homography net', warped_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
